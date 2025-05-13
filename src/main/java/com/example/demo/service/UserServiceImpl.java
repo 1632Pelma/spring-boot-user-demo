277a40ae36062f6abe173ed472106aace15748c2
Pelma Monere Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-05-13 08:41:18 +0200

    feat(service): implement constructor DI

---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.repo.FakeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final FakeRepo fakeRepo;
+
+    @Autowired
+    public UserServiceImpl(FakeRepo fakeRepo) {
+        this.fakeRepo = fakeRepo;
+    }
+
+}

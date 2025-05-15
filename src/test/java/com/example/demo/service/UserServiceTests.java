0b1b435da6359c7e53127c4fca68f5d99f7daf98
Pelma Monere Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-05-15 15:13:14 +0200

    Add UserServiceTests.java in service test package

---
+++ src/test/java/com/example/demo/service/UserServiceTests.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTests {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}

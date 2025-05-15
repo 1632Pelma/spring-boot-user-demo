e6a1eb446b40d2b234a15ea7a39fa7269f2a6192
Pelma Monere Mahlobogoane <125448084+1632Pelma@users.noreply.github.com>
2025-05-15 15:36:48 +0200

    test: verify user creation

--- src/test/java/com/example/demo/service/UserServiceTests.java
+++ src/test/java/com/example/demo/service/UserServiceTests.java
@@ -1,6 +1,14 @@
 package com.example.demo.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -10,5 +18,17 @@ class UserServiceTests {
 
     @InjectMocks
     private UserServiceImpl userService;
+
+    
+
+    @Test
+    void addUser_shouldReturnConfirmation() {
+        when(fakeRepo.insertUser(anyLong(), anyString(), anyString()))
+            .thenReturn("TestUser");
+        
+        String result = userService.addUser("TestUser", "TestSurname");
+        assertEquals("TestUser added", result);
+    }
+
 
 }
